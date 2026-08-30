3c6b14412d9043d9735b3a90883e615e9fd33342
1967600293 <84280407+1967600293@users.noreply.github.com>
2026-08-30 18:56:32 +0800

    update

--- settings.py
+++ settings.py
@@ -6,14 +6,14 @@ collection_name="rag"
 persist_directory="./chroma_db"
 
 # spliter
-chunk_size= 1000
+chunk_size= 512
 chunk_overlap= 100
 separators =["\n\n","\n",".","!","?","。","！","？"," ",""]
 
-max_spliter_char_number= 1000  # 文本分割阈值
+max_spliter_char_number= 512  # 文本分割阈值
 
 # 相似度K值
-similarity_threshold =2     # 检索返回匹配的文档数量
+similarity_threshold =3    # 检索返回匹配的文档数量
 
 embedding_model_name="text-embedding-v4"
 chat_model_name="qwen3-max"
